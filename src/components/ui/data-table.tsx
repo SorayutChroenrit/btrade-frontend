96f79a9a995af7a983238887e9e4dabe9a57bbc5
Sorayut CH. <sorayutapple@gmail.com>
2025-03-11 21:48:10 +0700

    add pagnination to table

--- src/components/ui/data-table.tsx
+++ src/components/ui/data-table.tsx
@@ -22,6 +22,7 @@ import {
 } from "@/components/ui/table";
 import { Button } from "./button";
 import { Input } from "@/components/ui/input";
+import { DataTablePagination } from "../general/Pagination";
 
 interface DataTableProps<TData, TValue> {
   columns: ColumnDef<TData, TValue>[];
@@ -114,24 +115,7 @@ export function DataTable<TData, TValue>({
           </TableBody>
         </Table>
       </div>
-      <div className="flex items-center justify-end space-x-2 py-4">
-        <Button
-          variant="outline"
-          size="sm"
-          onClick={() => table.previousPage()}
-          disabled={!table.getCanPreviousPage()}
-        >
-          Previous
-        </Button>
-        <Button
-          variant="outline"
-          size="sm"
-          onClick={() => table.nextPage()}
-          disabled={!table.getCanNextPage()}
-        >
-          Next
-        </Button>
-      </div>
+      <DataTablePagination table={table} />
     </div>
   );
 }
